626d80d3af79d2cc45dfcc2ae43264ef448af996
wyang1006 <wyang@ctseinc.com>
2025-02-20 14:16:16 -0500

    Update test_aws.py

--- test_aws.py
+++ test_aws.py
@@ -14,7 +14,7 @@ st.title("PostgreSQL Data with Dask")
 
 selected_month = st.selectbox("select",['April 2023','October 2023','April 2024','October 2024'])
 Selected_route =st.selectbox("select",['100','22','103','3'])
-query = f"SELECT * FROM otp WHERE \"routeShortName\" = '{Selected_route}' AND \"month\" = '{selected_month}';"
+query = f"SELECT * FROM otp WHERE \"routeShortName\" = '{Selected_route}' AND \"month\" = '{selected_month}' LIMIT 100;"
 df = conn.query(query, ttl="10m")
 
 
