e33f1c9a21d8dd08976a70c5f3dfbfb0e91ea9f1
wyang1006 <wyang@ctseinc.com>
2025-02-20 13:44:32 -0500

    Update test_aws.py

--- test_aws.py
+++ test_aws.py
@@ -1,23 +1,13 @@
 import streamlit as st
-import dask.dataframe as dd
-import psycopg2
+
 
 import pandas as pd
 
 # Database connection function
-@st.cache_data
-def load_data(query):
-    conn = psycopg2.connect(
-        dbname="miamicoa",
-        user="postgres",
-        password="19871228@Zy",
-        host="localhost",
-        port="5432"
-    )
-    df = pd.read_sql(query, conn)  # Load using Pandas
-    conn.close()
-    ddf = dd.from_pandas(df, npartitions=4)  # Convert to Dask DataFrame
-    return ddf
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM mytable;', ttl="10m")
 
 # Streamlit UI
 st.title("PostgreSQL Data with Dask")
@@ -25,8 +15,9 @@ st.title("PostgreSQL Data with Dask")
 selected_month = st.selectbox("select",['April 2023','October 2023','April 2024','October 2024'])
 Selected_route =st.selectbox("select",['100','22','103','3'])
 query = f"SELECT * FROM otp WHERE \"routeShortName\" = '{Selected_route}' AND \"month\" = '{selected_month}';"
+df = conn.query(query, ttl="10m")
 
-data = load_data(query)
+
 st.write("First 10 rows:")
-st.dataframe(data.compute().head(10))
+st.dataframe(df.head(10))
 
